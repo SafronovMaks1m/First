7359adf24db4f886eb04adc40db62cc7decada3d
SafronovMaks1m <safranov01@list.ru>
2023-09-17 11:58:20 +0300

    Init

--- test.c
+++ test.c
@@ -3,5 +3,5 @@
 int main() {
     double b, d;
     scanf("%lf %lf", &b, &d);
-    printf("Cymma %lf, raznost %lf, proisvedenie %lf, chastnoe %lf", b+d, b-d, b*d, b/d);
+    printf("Cym %lf, raznost %lf, proisvedenie %lf, chastnoe %lf", b+d, b-d, b*d, b/d);
 }
